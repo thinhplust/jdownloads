028ad171326ee4f17aacf531f7f151c305a619a7
thinhplust <thinhplust@gmail.com>
2020-10-22 22:02:49 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+cd
+echo "-----> Install gclone"
+wget https://dl.dropboxusercontent.com/s/py5datuvmrhj3a8/gclone
+mv gclone /usr/bin/
+chmod +x /usr/bin/gclone
+echo "-----> Gclone Installed"
+
+echo "-----> Download SA"
+wget https://dl.dropboxusercontent.com/s/ak8qyti9ai4juva/accounts.zip
+unzip -o accounts.zip
+rm accounts.zip
+echo "-----> SA Downloaded"
+
+echo "-----> Download Gclone Config"
+cd ~
+mkdir -p /root/.config/rclone/
+wget "https://dl.dropboxusercontent.com/s/3luc4gbjlgx9nyp/rclone.conf" -O /root/.config/rclone/rclone.conf
+echo "-----> Gclone Config successfully"
+
+echo "-----> Install Tmux"
+apt install tmux
+echo "-----> Tmux installed successfully"
+gclone --version
+echo "----> Install Crontab, Auto move File"
+apt install cron -y
+apt install nano -y
+apt install nload -y
+apt install htop -y
+apt install zip -y
+mkdir /root/logs
+touch /root/logs/gclone-upload.log
+wget https://dl.dropboxusercontent.com/s/fa7j43o2r63rxmf/rclone-upload.sh
+chmod a+x rclone-upload.sh
+crontab -l | { cat; echo "* * * * * /root/rclone-upload.sh >/dev/null 2>&1"; } | crontab -
+/etc/init.d/cron start
+/etc/init.d/cron status
+echo "------> Crontab configured successfully"
